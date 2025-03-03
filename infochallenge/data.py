e8c7ac24a513985387efcceca97fb0324f318a7c
Pedro Castellon <m270978@usna.edu>
2025-03-03 15:13:32 -0500

    made the names more readable

--- infochallenge/data.py
+++ infochallenge/data.py
@@ -6,12 +6,12 @@ import plotly.express as px
 # print(june15)
 # summeravg= pd.merge(june5,june15,on='Station Name')
 # print(summeravg)
-june5=pd.read_csv('june5_2024updated.csv', delimiter="\t", encoding='utf16')
+june5=pd.read_csv('June_5_2024.csv', delimiter="\t", encoding='utf16')
 #june5["day"]= int(june5["AM Peak (Open-9:30am)"]+june5["Midday (9:30am-3pm)"]+june5["PM Peak (3pm-7pm)"]+june5["Evening (7pm-12am)"]+june5["Late Night (12am-Close)"])
 #need to add the columns here and for every data set
 print(june5)
 
-june15=pd.read_csv('june15_2024_updated.csv', delimiter="\t", encoding='utf16')
+june15=pd.read_csv('June_15_2024.csv', delimiter="\t", encoding='utf16')
 
 print(june15)
 
@@ -27,8 +27,8 @@ juneteenth=pd.read_csv('June_19_2024.csv', delimiter="\t", encoding='utf16')
 
 print(juneteenth)
 
-summeravg=pd.read_csv('tabsep.csv', delimiter="\t", encoding='utf16')
+summer=pd.read_csv('all_summer.csv', delimiter="\t", encoding='utf16')
 
-print(summeravg)
+print(summer)
 
 # px.histogram(summeravg, x='Station Name', y='Ridership', nbins=24, width=1000, height=600)
