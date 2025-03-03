afdbe92860ee6ddbafdb9ab607ee9f76be7530ea
Pedro Castellon <m270978@usna.edu>
2025-03-03 08:47:48 -0500

    pedro

---
+++ infochallenge/data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+# june5=pd.read_csv ('June_05_2024.csv')
+# june15=pd.read_csv ('June_05_2024.csv')
+# print(june5)
+# print(june15)
+# summeravg= pd.merge(june5,june15,on='Station Name')
+# print(summeravg)
+
+summer=pd.read_csv('tabsep.csv', delimiter="\t", encoding='utf16')
+print(summer)
