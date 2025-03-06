ec52891fb6387c23f65aa08dbcc5257d79f969b3
m276984 <m276984@usna.edu>
2025-03-06 11:10:03 -0500

    changes

--- infochallenge/charts.py
+++ infochallenge/charts.py
@@ -1,16 +1,20 @@
 import plotly.express as px 
 import pandas as pd
 
+# Load the CSV file
+june19th = pd.read_csv('July_20_2024.csv', delimiter="\t", encoding='utf16')
 
-july27th = pd.read_csv('July_27_2024.csv', delimiter="\t", encoding='utf16')
+# Calculate the 'total' ridership across all time periods for each row (station)
+june19th["total"] = (
+    pd.to_numeric(june19th["AM Peak (Open-9:30am)"]) + 
+    pd.to_numeric(june19th["Midday (9:30am-3pm)"]) + 
+    pd.to_numeric(june19th["PM Peak (3pm-7pm)"]) + 
+    pd.to_numeric(june19th["Evening (7pm-12am)"]) + 
+    pd.to_numeric(june19th["Late Night (12am-Close)"])
+)
 
-
-df = pd.DataFrame(july27th)
-# Convert the DataFrame into a long format
-df_melted = df.melt(id_vars='Station Name', var_name='Time Period', value_name='Ridership')
-
-# Create a bar chart
-fig = px.bar(df_melted, x='Station Name', y='Ridership', color='Time Period', barmode='group', title='Ridership Across Different Time Periods for Each Station')
-
+# Create a bar chart using Plotly, where each time period is represented as a separate bar
+#fig = px.bar(june19th, x='Station Name', y= ['AM Peak (Open-9:30am)', 'Midday (9:30am-3pm)', 'PM Peak (3pm-7pm)', 'Evening (7pm-12am)', 'Late Night (12am-Close)', 'total'], barmode = 'group', title='Ridership Across Different Time Periods for Each Station')
+fig = px.bar(june19th, x='Station Name', y= 'total', barmode = 'group', title='Ridership Across Different Time Periods for Each Station')
 # Show the chart
 fig.show()
